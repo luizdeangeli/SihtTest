1a4cc253321ebd31d7d5982e2bee5b64c15ade80
luiz.angeli <luiz.angeli@unicesumar.edu.br>
2015-09-17 14:00:18 -0300

    update test

--- Application/City/Controller.php
+++ Application/City/Controller.php
@@ -2,10 +2,6 @@
 
 namespace Application\City;
 
-class Controller extends \Siht\Controller {
-
-    public function __construct() {
-        parent::__construct(__NAMESPACE__);
-    }
+class Controller extends \Siht\Controller {    
 
 }
